f5c64a200c8b1a5854c116e2d2ddb8c17f443dec
ДЕНИС <52046838+botkoda@users.noreply.github.com>
2021-10-04 09:22:18 +0400

    Update MyThread.java
    refactor code

--- src/MyThread.java
+++ src/MyThread.java
@@ -1,18 +1,16 @@
 import java.util.concurrent.Callable;
 
 class MyThread implements Callable<Integer> {
-    private  int countThread;
-    public MyThread(int countThread){
-        this.countThread=countThread;
+    private int countThread;
+
+    public MyThread(int countThread) {
+        this.countThread = countThread;
     }
 
     @Override
     public Integer call() throws Exception {
-
-                Thread.sleep(2500);
-                System.out.println(Thread.currentThread().getName()+ " Всем привет!");
-
-
+        Thread.sleep(2500);
+        System.out.println(Thread.currentThread().getName() + " Всем привет!");
         return countThread++;
     }
 
